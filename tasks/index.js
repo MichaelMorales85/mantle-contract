1c64947acfdb15b9967aa16a8661321d60892819
Eric <2461555172@qq.com>
2024-12-08 17:04:04 +0800

    init project

--- tasks/index.js
+++ tasks/index.js
@@ -1,7 +1,4 @@
 module.exports = {
     checkNft: require("./check-nft"),
     mintNft: require("./mint-nft"),
-    lockAndCross: require("./lock-and-cross"),
-    checkWrappedNft: require("./check-wrapped-nft"),
-    burnAndCross: require("./burn-and-cross"),
 }
